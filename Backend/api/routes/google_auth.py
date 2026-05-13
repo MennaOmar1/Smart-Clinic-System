28888e138740c5330b4b69db8cb924ab79796437
menna omar <omarmenna041@gmail.com>
2026-05-13 10:02:17 +0300

    fix deployment

--- Backend/api/routes/google_auth.py
+++ Backend/api/routes/google_auth.py
@@ -24,7 +24,7 @@ async def login(request: Request):
 
     redirect_uri = os.getenv(
         "GOOGLE_CALLBACK_URL",
-        "http://127.0.0.1:8000/auth/google/callback"
+        "https://smart-clinic-system-production.up.railway.app/auth/google/callback"
     )
 
     return await oauth.google.authorize_redirect(
